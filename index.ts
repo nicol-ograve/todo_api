d4f9b7bafbe9aa76209d9fe3bb4be9d4bc880ffa
Nicolò Sordoni <nsor@bendingspoons.com>
2025-04-13 15:49:31 +0200

    add post API

--- index.ts
+++ index.ts
@@ -13,6 +13,15 @@ fastify.get("/todos", async (request: any, reply: any) => {
   return rows;
 });
 
+fastify.post("/todos", async (request: any, reply: any) => {
+  const { id, text } = request.body;
+  const { rows } = await fastify.pg.query(
+    "INSERT INTO todos (id, text) VALUES ($1, $2) RETURNING *",
+    [id, text],
+  );
+  return rows[0];
+});
+
 fastify.listen({ host: "0.0.0.0", port: 8080 }, (err: any, address: any) => {
   if (err) {
     console.error(err);
