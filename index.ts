4464735c5b723b035dadb508c0fd6ab7c55164ab
Nicolò Sordoni <nsor@bendingspoons.com>
2025-04-13 15:22:57 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+const fastify = require("fastify")();
+const fastifyPostgres = require("@fastify/postgres");
+
+// DB plugin
+fastify.register(fastifyPostgres, {
+  connectionString: process.env.DATABASE_URL,
+});
+
+fastify.get("/todos", async (request: any, reply: any) => {
+  const client = await fastify.pg.connect();
+  const { rows } = await client.query("SELECT * FROM todos");
+  client.release();
+  return rows;
+});
+
+fastify.listen({ port: 8080 }, (err: any, address: any) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
